bc7dbf1af6ade16ed4fe2cd7152bf7d03716aa88
Richard Bång <richard.bang@live.se>
2018-07-13 09:26:20 +0200

    home swiper fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,31 +14,37 @@ jQuery(document).ready( function($) {
         grabCursor: true
     });
 
-    //HOME HERO SWIPER MIDDLE
-    var homeHeroSwiperMiddle = new Swiper('.cd-home-swiper-middle', {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 0,
-        autoplay: 4500,
-        loop: true,
-        slideShadows: true,
-        disableOnInteraction: false,
-        effect: 'flip',
-        grabCursor: true
-    });
+    setTimeout(function(){
+        //HOME HERO SWIPER MIDDLE
+        var homeHeroSwiperMiddle = new Swiper('.cd-home-swiper-middle', {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0,
+            autoplay: 4000,
+            loop: true,
+            slideShadows: true,
+            disableOnInteraction: false,
+            effect: 'flip',
+            grabCursor: true
+        });
+    }, 500);
 
-    //HOME HERO SWIPER RIGHT
-    var homeHeroSwiperRight = new Swiper('.cd-home-swiper-right', {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 0,
-        autoplay: 4250,
-        loop: true,
-        slideShadows: true,
-        disableOnInteraction: false,
-        effect: 'flip',
-        grabCursor: true
-    });
+
+    setTimeout(function(){
+        //HOME HERO SWIPER RIGHT
+        var homeHeroSwiperRight = new Swiper('.cd-home-swiper-right', {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0,
+            autoplay: 4000,
+            loop: true,
+            slideShadows: true,
+            disableOnInteraction: false,
+            effect: 'flip',
+            grabCursor: true
+        });
+    }, 1000);
+
 
     $('div.cone-tr .sod_select').on('focusout', function (e) {
         $('.cone-tr').removeClass('open');
