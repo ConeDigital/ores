7519b962689b44a29625bf9d3f2e577f0de15936
Richard Bång <richard.bang@live.se>
2018-07-16 10:57:03 +0200

    size guide and home swiper fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,57 @@
 jQuery(document).ready( function($) {
 
+    //Initiate swipers after load
+    $(window).load(function () {
+        //HOME HERO SWIPER LEFT
+        var homeHeroSwiperLeft = new Swiper('.cd-home-swiper-left', {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0,
+            autoplay: 6000,
+            loop: true,
+            slideShadows: true,
+            disableOnInteraction: false,
+            effect: 'flip',
+            grabCursor: true
+        });
 
-    //HOME HERO SWIPER LEFT
-    var homeHeroSwiperLeft = new Swiper('.cd-home-swiper-left', {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 0,
-        autoplay: 6000,
-        loop: true,
-        slideShadows: true,
-        disableOnInteraction: false,
-        effect: 'flip',
-        grabCursor: true
+        //HOME HERO SWIPER MIDDLE
+        var homeHeroSwiperMiddle = new Swiper('.cd-home-swiper-middle', {
+            init: false,
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0,
+            autoplay: 6000,
+            loop: true,
+            slideShadows: true,
+            disableOnInteraction: false,
+            effect: 'flip',
+            grabCursor: true
+        });
+
+        //HOME HERO SWIPER RIGHT
+        var homeHeroSwiperRight = new Swiper('.cd-home-swiper-right', {
+            init: false,
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0,
+            autoplay: 6000,
+            loop: true,
+            slideShadows: true,
+            disableOnInteraction: false,
+            effect: 'flip',
+            grabCursor: true
+        });
+
+        setTimeout(function(){
+            homeHeroSwiperMiddle.init();
+        }, 500);
+
+
+        setTimeout(function(){
+            homeHeroSwiperRight.init();
+        }, 1000);
     });
-
-    //HOME HERO SWIPER MIDDLE
-    var homeHeroSwiperMiddle = new Swiper('.cd-home-swiper-middle', {
-        init: false,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 0,
-        autoplay: 6000,
-        loop: true,
-        slideShadows: true,
-        disableOnInteraction: false,
-        effect: 'flip',
-        grabCursor: true
-    });
-
-    //HOME HERO SWIPER RIGHT
-    var homeHeroSwiperRight = new Swiper('.cd-home-swiper-right', {
-        init: false,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 0,
-        autoplay: 6000,
-        loop: true,
-        slideShadows: true,
-        disableOnInteraction: false,
-        effect: 'flip',
-        grabCursor: true
-    });
-
-    setTimeout(function(){
-        homeHeroSwiperMiddle.init();
-    }, 500);
-
-
-    setTimeout(function(){
-        homeHeroSwiperRight.init();
-    }, 1000);
 
 
     $('div.cone-tr .sod_select').on('focusout', function (e) {
