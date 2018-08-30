eea81118d31c845aa9f1c501d19233e83a595e2e
Richard Bång <richard.bang@live.se>
2018-08-30 15:47:30 +0200

    email pdf attachment

--- functions.php
+++ functions.php
@@ -14,5 +14,15 @@
 	add_action( 'wp_enqueue_scripts', 'nm_child_theme_styles', 1000 ); // Note: Use priority "1000" to include the stylesheet after the parent theme stylesheets
 
 
-remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
-add_action('woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 45);
+	remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
+	add_action('woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 45);
+
+	//Add pdf to processing email
+	add_filter('woocommerce_email_attachments', 'attach_terms_conditions_pdf_to_email', 10,  3);
+	function attach_terms_conditions_pdf_to_email ( $attachments, $status ,  $order ) {
+		$allowed_statuses = array('customer_processing_order');
+		if( isset( $status ) && in_array ( $status, $allowed_statuses ) ) {
+			$attachments[] = get_stylesheet_directory() . '/assets/images/withdrawal.pdf';
+		}
+		return $attachments;
+	}
